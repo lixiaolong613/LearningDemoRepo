f29716b25bd1b8ce4958b49d8e44f67286e282a7
lilong <lilong@bigo.sg>
2022-11-11 16:16:02 +0800

    add binding in main

--- MadsLearningProj/app/src/main/java/com/example/madslearning/MainActivity.kt
+++ MadsLearningProj/app/src/main/java/com/example/madslearning/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.madslearning
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.example.madslearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val TAG = "MainActivityLog"
+    }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
     }
 }
